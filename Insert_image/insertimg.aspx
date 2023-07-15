1093e9425a67cdb5d8aa839484f7b0f70fb6a198
Parth sata <parthsata1709@gmail.com>
2023-07-15 20:03:29 +0530

    Add project files.

---
+++ Insert_image/insertimg.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="insertimg.aspx.cs" Inherits="Insert_image.insertimg" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script>
+        function ShowPreview() {/*
+            let url = document.getElementById("FileUpoad1")
+            let filePath = url.value;*/
+            let fileInput = document.getElementById("FileUpload1");
+            let filePath = fileInput.value; // Retrieve the file path
+            alert(filePath)
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="FileUpload1" onchange="ShowPreview(this)" runat="server" />
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Insert_image/insertimg.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Insert_image
+{
+    public partial class insertimg : System.Web.UI.Page
+    {
+        SqlConnection cn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=F:\\Parth\\Insert_image\\Insert_image\\App_Data\\images.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string fileExtension = System.IO.Path.GetExtension(FileUpload1.FileName);
+            if (fileExtension == ".pdf" || fileExtension == ".jpg")
+            {
+                FileUpload1.SaveAs(Server.MapPath("images/" + FileUpload1.FileName));
+                string sql = "insert into images values('" + FileUpload1.FileName + "')";
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, cn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                Response.Write("Inserted");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
