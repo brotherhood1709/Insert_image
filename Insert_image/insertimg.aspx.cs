488dfbae73bcb3fa24b9cecfc1bce9f5ce0f2276
Parth sata <parthsata1709@gmail.com>
2023-07-15 20:08:42 +0530

    tabels

--- Insert_image/insertimg.aspx.cs
+++ Insert_image/insertimg.aspx.cs
@@ -35,5 +35,55 @@ namespace Insert_image
 
             }
         }
+        /*
+         CREATE TABLE [dbo].[Registration] (
+            [Id]               INT           NOT NULL,
+            [Date]             NCHAR (10)    NOT NULL,
+            [Party_Name]       NVARCHAR (50) NOT NULL,
+            [Consigment_No]    NVARCHAR (13) NOT NULL,
+            [Weight]           NCHAR (10)    NOT NULL,
+            [Total_Amount]     NVARCHAR (20) NOT NULL,
+            [Charges]          VARCHAR (50)  NOT NULL,
+            [To_Destination]   VARCHAR (50)  NOT NULL,
+            [From_Destination] VARCHAR (50)  NOT NULL,
+            CONSTRAINT [PK_Registration] PRIMARY KEY CLUSTERED ([Id] ASC)
+        );
+        CREATE TABLE [dbo].[Register_user] (
+    [First_Name]       VARCHAR (50)  NOT NULL,
+    [Last_Name]        VARCHAR (50)  NOT NULL,
+    [Email]            NCHAR (50)    NOT NULL,
+    [Username]         NVARCHAR (50) NOT NULL,
+    [Password]         NVARCHAR (50) NOT NULL,
+    [Confirm_Password] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Register_user] PRIMARY KEY CLUSTERED ([Username] ASC)
+);
+
+CREATE TABLE [dbo].[Login] (
+    [username] NVARCHAR (50) NOT NULL,
+    [password] NVARCHAR (50) NOT NULL
+);
+CREATE TABLE [dbo].[Courier_Entry] (
+    [Id]               INT           NOT NULL,
+    [Date]             NCHAR (10)    NOT NULL,
+    [Party_Name]       NVARCHAR (50) NOT NULL,
+    [Consignment _No]  NVARCHAR (13) NOT NULL,
+    [Weight]           NCHAR (10)    NOT NULL,
+    [Total_Amount]     NVARCHAR (20) NOT NULL,
+    [Charges]          NCHAR (10)    NOT NULL,
+    [To_Destination]   NVARCHAR (50) NOT NULL,
+    [From_Destination] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Courier_Entry] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Contact] (
+    [Your_name]    NVARCHAR (50)  NOT NULL,
+    [Your_email]   NVARCHAR (50)  NOT NULL,
+    [Subject]      NVARCHAR (50)  NOT NULL,
+    [Your_message] NVARCHAR (MAX) NOT NULL
+);
+
+
+
+         */
     }
 }
