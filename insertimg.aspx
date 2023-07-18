0c9351553c6a0a63cdd322bffac6d169592451af
Parth Sata <111201138+brotherhood1709@users.noreply.github.com>
2023-07-18 19:30:24 +0530

    Add files via upload

---
+++ insertimg.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="insertimg.aspx.cs" Inherits="Insert_image.insertimg" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script>
+        function ShowPreview() {
+            let fileInput = document.getElementById("FileUpload1");
+            let filePath = fileInput.value; // Retrieve the file path
+            // Get the image element by ID
+            let imageElement = document.getElementById("Image1");
+
+            // Set the source attribute of the image element
+            imageElement.src = window.URL.createObjectURL(fileInput.files[0]);
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server"  Height="200" Width="200"/>
+
+            <asp:FileUpload ID="FileUpload1" onchange="ShowPreview(this)" runat="server" />
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+            <asp:DataList ID="DataList1" runat="server" RepeatDirection="Horizontal" DataKeyField="Id" DataSourceID="SqlDataSource1" Font-Bold="False" Font-Italic="False" Font-Overline="False" Font-Size="Large" Font-Strikeout="False" Font-Underline="False" RepeatColumns="2" style="margin-right: 26px">
+                <ItemTemplate>
+                    Id:
+                    <asp:Label ID="IdLabel" runat="server" Text='<%# Eval("Id") %>' />
+                    <br />
+                    images:<asp:Image ID="Image2" runat="server" Height="115px" Width="123px" />
+                    <br />
+                    <br />
+                </ItemTemplate>
+            </asp:DataList>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [images]"></asp:SqlDataSource>
+        </div>
+    </form>
+</body>
+</html>
---
+++ insertimg.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Insert_image
+{
+    public partial class insertimg : System.Web.UI.Page
+    {
+        SqlConnection cn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=F:\\Parth\\Insert_image\\Insert_image\\App_Data\\images.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string fileExtension = System.IO.Path.GetExtension(FileUpload1.FileName);
+            if (fileExtension == ".pdf" || fileExtension == ".jpg")
+            {
+                FileUpload1.SaveAs(Server.MapPath("images/" + FileUpload1.FileName));
+                string sql = "insert into images values('"+FileUpload1.FileName+"')";
+                SqlDataAdapter da = new SqlDataAdapter(sql,cn);
+                DataSet dt = new DataSet();
+                DataList1.DataBind();
+                da.Fill(dt);
+                Fill();
+            }
+            else
+            {
+                Response.Write("Enter Valid Format");
+
+            }
+        }
+        public void Fill()
+        {
+            string sql = "select * from images";
+            SqlDataAdapter adapter = new SqlDataAdapter(sql, cn);
+            DataSet dt = new DataSet();
+            DataList1.DataBind();
+            adapter.Fill(dt);
+            Response.Write("Selected");
+        }
+        /*
+         CREATE TABLE [dbo].[Registration] (
+            [Id]               INT           NOT NULL,
+            [Date]             NCHAR (10)    NOT NULL,
+            [Party_Name]       NVARCHAR (50) NOT NULL,
+            [Consigment_No]    NVARCHAR (13) NOT NULL,
+            [Weight]           NCHAR (10)    NOT NULL,
+            [Total_Amount]     NVARCHAR (20) NOT NULL,
+            [Charges]          VARCHAR (50)  NOT NULL,
+            [To_Destination]   VARCHAR (50)  NOT NULL,
+            [From_Destination] VARCHAR (50)  NOT NULL,
+            CONSTRAINT [PK_Registration] PRIMARY KEY CLUSTERED ([Id] ASC)
+        );
+        CREATE TABLE [dbo].[Register_user] (
+    [First_Name]       VARCHAR (50)  NOT NULL,
+    [Last_Name]        VARCHAR (50)  NOT NULL,
+    [Email]            NCHAR (50)    NOT NULL,
+    [Username]         NVARCHAR (50) NOT NULL,
+    [Password]         NVARCHAR (50) NOT NULL,
+    [Confirm_Password] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Register_user] PRIMARY KEY CLUSTERED ([Username] ASC)
+);
+
+CREATE TABLE [dbo].[Login] (
+    [username] NVARCHAR (50) NOT NULL,
+    [password] NVARCHAR (50) NOT NULL
+);
+CREATE TABLE [dbo].[Courier_Entry] (
+    [Id]               INT           NOT NULL,
+    [Date]             NCHAR (10)    NOT NULL,
+    [Party_Name]       NVARCHAR (50) NOT NULL,
+    [Consignment _No]  NVARCHAR (13) NOT NULL,
+    [Weight]           NCHAR (10)    NOT NULL,
+    [Total_Amount]     NVARCHAR (20) NOT NULL,
+    [Charges]          NCHAR (10)    NOT NULL,
+    [To_Destination]   NVARCHAR (50) NOT NULL,
+    [From_Destination] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Courier_Entry] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Contact] (
+    [Your_name]    NVARCHAR (50)  NOT NULL,
+    [Your_email]   NVARCHAR (50)  NOT NULL,
+    [Subject]      NVARCHAR (50)  NOT NULL,
+    [Your_message] NVARCHAR (MAX) NOT NULL
+);
+
+
+
+         */
+    }
+}
